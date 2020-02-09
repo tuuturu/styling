f6cb55db13916e7af94b3b251ce157f569fbf674
Julius Pedersen <deifyed@gmail.com>
2020-02-09 12:26:49 +0100

    ✅ ADD: gulp build scripts

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { series, src, dest } = require('gulp')
+const concat = require('gulp-concat')
+const del = require('delete')
+
+function buildStyles() {
+	return src([
+		'source/scss/variables.scss',
+		'source/scss/palette.scss',
+		'source/scss/mixins.scss',
+		'source/scss/base.scss',
+	])
+		.pipe(concat('style.scss'))
+		.pipe(dest('dist/'))
+}
+
+function clean(cb) {
+	del(['dist/'], cb)
+}
+
+exports.build = buildStyles
+exports.clean = clean
+exports.default = series(clean, buildStyles)
